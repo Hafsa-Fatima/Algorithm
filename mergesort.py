82e2899153488949c10dd5f1662ad8f7134e604a
hafsa-fatima <hafsa.fatima995@gmail.com>
2018-09-23 23:35:53 -0500

    changes in quick and merge

--- mergesort.py
+++ mergesort.py
@@ -1,75 +1,67 @@
 import time
+def mergeSort(A):
+    if len(A)>1:
+        mid = len(A)//2
+        L = A[:mid]
+        R = A[mid:]
 
-def mergeSort(A,p,r): 
-    if p < r: 
-  
-        q = int((p+(r-1))/2)
-        #recursively call merge sort split array until it contain one item
-        mergeSort(A, p, q) 
-        mergeSort(A, q+1, r) 
-        #merge items
-        merge(A, p, q, r) 
+        #recursively calling mergesort 
+        mergeSort(L)
+        mergeSort(R)
+        merge(A,L,R)
 
-def merge(A, p, q, r): 
-    n1 = q - p + 1
-    n2 = r- q 
+def merge(A,L,R):
+    i=0
+    j=0
+    k=0
+    
+    while i < len(L) and j < len(R):
+        if L[i] < R[j]:
+            A[k]=L[i]
+            i=i+1
+        else:
+            A[k]=R[j]
+            j=j+1
+        k=k+1
 
-    L = [0] * (n1) 
-    R = [0] * (n2) 
+    while i < len(L):
+        A[k]=L[i]
+        i=i+1
+        k=k+1
 
-    for i in range(0 , n1): 
-        L[i] = A[p + i ] 
-  
-    for j in range(0 , n2): 
-        R[j] = A[q + j + 1] 
-  
-     
-    i = 0     
-    j = 0     
-    k = p     
-  
-    while i < n1 and j < n2 : 
-        if L[i] <= R[j]: 
-            A[k] = L[i] 
-            i += 1
-        else: 
-            A[k] = R[j] 
-            j += 1
-        k += 1
-  
-    while i < n1: 
-        A[k] = L[i] 
-        i += 1
-        k += 1
-  
-    while j < n2: 
-        A[k] = R[j] 
-        j += 1
-        k += 1
+    while j < len(R):
+        A[k]=R[j]
+        j=j+1
+        k=k+1
 
 def main():
 
-    A = list()
     #input size of array from the user 
     print("Choose a size of array : ") 
     n=input()
+
     print("enter array items: ")  
     for i in range(0,int(n)):
         j=input()
         A.append(int(j))
+
     #CPU start time
     stime=time.clock()
-    mergeSort(A,0,int(n)-1) 
+
+    mergeSort(A) 
+
     #CPU end time
     etime=time.clock()
+
     print ("MergeSort result is:") 
-    for i in A: 
-        print (i,end=' ') 
+    print (A) 
+    
     #Total time complexity
     Ttime=etime-stime
-    print()
+
     print("computation time = "+str(Ttime))
 
 #call main function
+A = list()
 if __name__ == "__main__":
   main()
--- quicksort.py
+++ quicksort.py
@@ -4,9 +4,12 @@ def quickSort(A,start,end):
 
     # start is starting index 
     # end is last index 
+
     if start < end: 
+
         # p is pivot index
         p = partition(A,start,end) 
+
         #call quicksort recursively
         quickSort(A, start, p-1) 
         quickSort(A, p+1, end) 
@@ -14,38 +17,48 @@ def quickSort(A,start,end):
 # Partion function
 def partition(A,start,end): 
 
-    i = ( start-1 )    
+    i = ( start-1 )  
+
     #assign last index array item as pivot    
     pivot = A[end]      
+
     for j in range(start , end): 
         if   A[j] <= pivot: 
             i = i+1 
+
             #swap ith and jth items
-            A[i],A[j] = A[j],A[i] 
+            A[i],A[j] = A[j],A[i]
+
     #swap the pivot to its correct position
     A[i+1],A[end] = A[end],A[i+1] 
+    
     return ( i+1 ) 
 
 # main function
 def main():
+
     #input size of array from the user 
     print("Choose A size of array : ") 
     n=input()
+
     print("enter array items: ")  
     for i in range(0,int(n)):
         j=input()
         A.append(int(j))
+
     #CPU start time
     stime=time.clock()
+
     quickSort(A,0,int(n)-1) 
+
     #CPU end time
     etime=time.clock()
     print ("QuickSort result is:") 
-    for i in A: 
-        print (i,end=' ') 
+
+    print(A)
+    
     #Total time complexity
     Ttime=etime-stime
-    print()
     print("computation time = "+str(Ttime))
 
 #call main function
